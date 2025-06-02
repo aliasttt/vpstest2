c91049404f93e97c3b12777b0003a6dcbfbae80e
aliasttt <aliasadi3853@gmail.com>
2025-06-02 23:49:12 +0300

    last update

--- vpstest/settings.py
+++ vpstest/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vpstest2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
